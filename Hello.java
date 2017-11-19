393aeb909b2c41743fccbd29603345ecee08fec0
Daniel Persson <mailto.woden@gmail.com>
2017-11-19 20:49:22 +0100

    More lines to java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] argv) {
         System.out.println("Hello everyone");
         System.out.println("Hello explorers");
+        System.out.println("Hi fine people");
     }
 }
